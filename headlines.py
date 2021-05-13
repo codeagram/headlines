645a819f00c53b7b0e51aac689acb5fe62fc564a
arunkumar <arunmlr.jaysel@gmail.com>
2021-05-13 20:37:02 +0530

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,23 +1,30 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-RSS_FEEDS = {'BBC': 'http://feeds.bbci.co.uk/news/rss.xml',
-            'LifeHacker': 'https://lifehacker.com/rss',
-            'Wired': 'https://www.wired.com/feed/rss',
-            'HackerNoon': 'https://hackernoon.com/feed',
-            'DarkHacker': 'http://feeds.feedburner.com/Darkhackerworld',
-            'HackADay': 'https://hackaday.com/blog/feed/',
-            'HowToGeek': 'http://feeds.howtogeek.com/HowToGeek'
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+            'lifehacker': 'https://lifehacker.com/rss',
+            'wired': 'https://www.wired.com/feed/rss',
+            'hackernoon': 'https://hackernoon.com/feed',
+            'darkhacker': 'http://feeds.feedburner.com/Darkhackerworld',
+            'hackaday': 'https://hackaday.com/blog/feed/',
+            'howtogeek': 'http://feeds.howtogeek.com/HowToGeek'
             }
 
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication='BBC'):
+def get_news():
+
+    query = request.args.get('publication')
+
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'bbc'
+
+    else:
+        publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
 
